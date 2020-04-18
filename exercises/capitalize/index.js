97c112ef5e3cfc89a0e6441557b4a5f1c5a343b1
Lee Tsai <lee.x.tsai@gmail.com>
2020-04-18 16:59:36 -0400

    solve capitalize with split and slice

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,19 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const arr = str.split(" ");
+    const words = [];
+
+    for (word of arr) {
+        words.push(capIt(word))
+    }
+
+    return words.join(" ");
+}
+
+function capIt(str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
 
 module.exports = capitalize;
