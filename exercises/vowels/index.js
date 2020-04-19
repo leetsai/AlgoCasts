10bfd0cda4d9d1e3a7db86b7962fc237ead245d1
Lee Tsai <lee.x.tsai@gmail.com>
2020-04-18 22:58:01 -0400

    implement regex solution using match

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,23 +8,29 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-    const vowelMap = {
-        "a": 1,
-        "e": 1,
-        "i": 1,
-        "o": 1,
-        "u": 1,
-    }
+    const matches = str.match(/[aeiou]/gi);
 
-    let count = 0;
-
-    for (char of str.toLowerCase()) {
-        if (vowelMap[char]) {
-            count++;
-        }
-    }
-
-    return count;
+    return matches ? matches.length : 0;
 }
+
+// function vowels(str) {
+//     const vowelMap = {
+//         "a": 1,
+//         "e": 1,
+//         "i": 1,
+//         "o": 1,
+//         "u": 1,
+//     }
+
+//     let count = 0;
+
+//     for (char of str.toLowerCase()) {
+//         if (vowelMap[char]) {
+//             count++;
+//         }
+//     }
+
+//     return count;
+// }
 
 module.exports = vowels;
