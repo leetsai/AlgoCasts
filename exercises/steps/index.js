9bb221a291f8ff7010ce061ac33eaf2884d6f8c2
Lee Tsai <lee.x.tsai@gmail.com>
2020-04-18 21:50:02 -0400

    solve using iterative loop

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,20 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    for (let i = 1; i <= n; i++) {
+        let step = "";
+
+        for (let j = 1; j <= n; j++) {
+            if (j <= i) {
+                step += "#";
+            } else {
+                step += " ";
+            }
+        }
+
+        console.log(step);
+    }
+}
 
 module.exports = steps;
