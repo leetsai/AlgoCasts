c2bd4de5d92bdf5453264d8d8dbf4c571670b214
Lee Tsai <lee.x.tsai@gmail.com>
2020-04-18 22:53:55 -0400

    use object to look up vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,24 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const vowelMap = {
+        "a": 1,
+        "e": 1,
+        "i": 1,
+        "o": 1,
+        "u": 1,
+    }
+
+    let count = 0;
+
+    for (char of str.toLowerCase()) {
+        if (vowelMap[char]) {
+            count++;
+        }
+    }
+
+    return count;
+}
 
 module.exports = vowels;
